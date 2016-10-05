298a8d5669a4d333e12342ff9afa876153e151d7
aswathyachu <aswathybabu455@gmail.com>
2016-10-05 20:08:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,63 +4,11 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var Articleone={
-    title:'aswathybabu',
-    heading:'Article-one',
-    date: 'Oct 1 2016',
-   content:`
-<p>
-        This is the first article of the project.This is the first article of the project.This is the first article of the project.This is the first article of the project.This is the first article of the project.This is the first article of the project.This is the first article of the project.
-</p>
-<p>
-        This is the first article of the project.This is the first article of the project.This is the first article of the project.This is the first article of the project.This is the first article of the project.This is the first article of the project.This is the first article of the project.
-</p>
-<p>
-        This is the first article of the project.This is the first article of the project.This is the first article of the project.This is the first article of the project.This is the first article of the project.This is the first article of the project.This is the first article of the project.
-      content:'
- </p> `
-};
-function createTemplate (data)
-{
-var title=data.title;
-var heading=data.heading;
-var date=data.date;
-var content=data.content;
-
-var  htmltemplate=`
-    <html>
-    <head>
-<title>${title}
-</title>
-</head>
-        <body>
-             <div class="container">
-        <div>
-            <a href='/home'></a>
-            </div>
-            <h3>
-            $ {heading}
-        </h3>
-    </div>
-        <div>
-            $ {date}
-        </div>
-<div>
-   
-    <p>
-        $ {content}
-     </p>
-    </div>
-    </body>
-    </html>
-';
-    return htmlTemplate;
-}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function (req,res) {
-    res.send(create Template(Articleone));
+    res.send(path.join(__dirname,'ui','article-one.html'));
     });
  
 app.get('/article-two',function (req,res) {
@@ -84,4 +32,3 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-}
